539ae6d290b1c779b37487d8448bfa31aa946dec
housheng <764141889@qq.com>
2017-08-16 21:49:53 +0800

    messageBox

--- JS/tool.js
+++ JS/tool.js
@@ -12,3 +12,12 @@ function get_style(str,arr) {
         return parseInt(getComputedStyle(str,false)[arr])
     }
 }
+function moving(top_val,color){
+    if(document.all){
+        event.cancelBubble=true;
+    }else{
+        event.stopPropagation();
+    }
+    message.style.top=top_val+"px";
+    content.style.backgroundColor=color;
+}
