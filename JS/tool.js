9e8f4e10b834b3d5ed885b08307e720f74fa41b2
borkenegg <764141889@qq.com>
2017-08-18 17:35:38 +0800

    v0.0.3

--- JS/tool.js
+++ JS/tool.js
@@ -4,20 +4,40 @@
 function get_id(id) {
     return document.getElementById(id)?document.getElementById(id):null;
 }
-function get_style(str,arr) {
+function getStyle(obj,attr){
     if(document.all){
-        return parseInt(str.currentStyle[arr]);
+        return parseInt(obj.currentStyle[attr]);
     }
-    else {
-        return parseInt(getComputedStyle(str,false)[arr])
+    else{
+        return parseInt(getComputedStyle(obj)[attr]);
     }
 }
-function moving(top_val,color){
+
+//function moving(){
+//    clearInterval(timer);
+//    timer=setInterval(function() {
+//        var top_val = getStyle(message, "top");
+//        console.log(top_val);
+//        if (top_val > 146) {
+//            top_val += 5;
+//        }else{
+//            clearInterval(timer);
+//        }
+//        message.style.top=top_val+"px";
+//    },17);
+//}
+
+
+
+//function moving(top_val,color){
     if(document.all){
         event.cancelBubble=true;
     }else{
         event.stopPropagation();
     }
-    message.style.top=top_val+"px";
-    content.style.backgroundColor=color;
-}
+
+//
+//
+//    message.style.top=top_val+"px";
+//    content.style.backgroundColor=color;
+//}
